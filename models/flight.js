076b10dfa62906446223caac9878ae3266d6be2a
Damian <bzdyrad@gmail.com>
2022-09-23 16:38:01 -0400

    update flightSchema and create Flight model

--- models/flight.js
+++ models/flight.js
@@ -2,25 +2,34 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const Flight = new Schema({
-  airline: {
-    type: String,
-    enum: ["American", "Southwest", "United"],
-  },
-  airport: {
-    type: String,
-    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
-    default: "DEN",
-  },
-  flightNo: {
-    type: Number,
-    min: 10,
-    max: 9999,
-  },
-  departs: {
-    type: Date,
-    default: function () {
-      return Date.now() + 1000 * 60 * 60 * 24 * 365;
+const flightSchema = new Schema(
+  {
+    airline: {
+      type: String,
+      enum: ["American", "Southwest", "United"],
+    },
+    airport: {
+      type: String,
+      enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+      default: "DEN",
+    },
+    flightNo: {
+      type: Number,
+      min: 10,
+      max: 9999,
+    },
+    departs: {
+      type: Date,
+      default: function () {
+        return Date.now() + 1000 * 60 * 60 * 24 * 365;
+      },
     },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const Flight = mongoose.model("Flight", flightSchema);
+
+export { Flight };
