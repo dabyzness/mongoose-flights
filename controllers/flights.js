0d7c25e2dd58e807b3bf2d93c0597571e000a897
Damian <bzdyrad@gmail.com>
2022-09-24 13:58:00 -0400

    implement functionality to edit flight

--- controllers/flights.js
+++ controllers/flights.js
@@ -42,4 +42,51 @@ function deleteFlight(req, res) {
     });
 }
 
-export { index, newFlight as new, create, deleteFlight as delete };
+function show(req, res) {
+  Flight.findById(req.params.id)
+    .then((flight) => {
+      res.render("flights/show", {
+        flight,
+        title: `Flight ${flight.flightNo} Info`,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/flights");
+    });
+}
+
+function edit(req, res) {
+  Flight.findById(req.params.id)
+    .then((flight) => {
+      res.render("flights/edit", {
+        flight,
+        title: `Edit`,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/flights");
+    });
+}
+
+function submitEdit(req, res) {
+  Flight.findByIdAndUpdate(req.params.id, req.body)
+    .then((flight) => {
+      res.redirect(`/flights/${flight._id}`);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/flights");
+    });
+}
+
+export {
+  index,
+  newFlight as new,
+  create,
+  deleteFlight as delete,
+  show,
+  edit,
+  submitEdit,
+};
--- routes/flights.js
+++ routes/flights.js
@@ -6,10 +6,17 @@ const router = Router();
 /* GET */
 router.get("/", flightsCtrl.index);
 
+router.get("/:id", flightsCtrl.show);
+
+router.get("/:id/edit", flightsCtrl.edit);
+
 router.get("/new", flightsCtrl.new);
 
 /* POST */
 router.post("/", flightsCtrl.create);
+
+/* PUT */
+router.put("/:id", flightsCtrl.submitEdit);
 
 /* DELETE */
 router.delete("/:id", flightsCtrl.delete);
